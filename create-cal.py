472f5ad73851d792b61691400476f08a652727fa
Rohan Puthukudy <rohan@gleebee.com>
2020-12-22 15:08:46 -0800

    Finished data extraction from HTML

--- create-cal.py
+++ create-cal.py
@@ -4,29 +4,42 @@ with open('webregMain.html', 'r') as fi:
     htmltext = fi.read()
 
 soup = BeautifulSoup(htmltext, 'lxml')
-n = 0
+
+# Extracts rows of tables from html
+# This gets us rows from two different tables, however we only want the first table
 rows = soup.find_all(class_='ui-widget-content jqgrow ui-row-ltr wr-grid-en')
 missing_row = soup.find(class_='ui-widget-content jqgrow ui-row-ltr wr-grid-en ui-state-hover')
-rows.insert(int(missing_row['id']), missing_row)
+if missing_row:
+    rows.insert(int(missing_row['id']), missing_row)
 
-for r in rows:
-    print(r['id'])
+# Finds point where second table starts and slices our array
+i = 0
+while int(rows[i]['id']) == i:
+    i += 1
+table = rows[:i]
 
-for row in rows:
-    # This happens when the current row is part of the finals table on the calendar tab of the website
-    if int(row['id']) != n:
-        print(f'n={n}, id={row["id"]}')
-        break
+# Populates array of courses where each course consists of 3 row vectors
+courses = []
+curr_course = []
+for row in table:
 
-    # Every class has 3 rows corresponding to it
-    # This is actually not true, MATH 95 has no discussion so there is only 2 rows
-    if n%3 == 0:
-        print(f'\nClass {int(n/3)}:', end='')
-
-    # Prints out all the text in each row
+    # Converts row into an array of cell data
+    course_row = []
     for cell in row.findChildren(recursive=False):
         if cell['style'] != 'display:none;' and cell.text.strip() != '':
-            print(cell.text.strip(), end=' ')
+            course_row.append(cell.text.strip())
 
-    print()
-    n += 1
+    # Adds row to the current course
+    curr_course.append(course_row)
+
+    # When reached the last row corresponding to the current course, adds course to list of courses
+    if course_row[0] == 'Final Exam':
+        courses.append(curr_course)
+        curr_course = []
+
+for c in courses:
+    print(c[0][0])
+    for row in c:
+        print(row)
+
+# Converts 3D course array into an array of course dicts with relevant calendar info
